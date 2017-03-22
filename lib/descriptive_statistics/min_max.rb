ae4f2e256d46db09944622e4453bcc768ffe3155
John Pelly <jpelly@gmail.com>
2017-03-22 08:52:19 -0400

    provide a default value for #min_value

--- lib/descriptive_statistics/min_max.rb
+++ lib/descriptive_statistics/min_max.rb
@@ -1,14 +1,14 @@
 module DescriptiveStatistics
   def min_value(collection = self, &block)
     values = Support::convert(collection, &block)
-    return DescriptiveStatistics.min_empty_collection_default_value if values.empty?
+    return nil if values.empty?
 
     values.min
   end
 
   def max_value(collection = self, &block)
     values = Support::convert(collection, &block)
-    return DescriptiveStatistics.max_empty_collection_default_value if values.empty?
+    return nil if values.empty?
 
     values.max
   end
